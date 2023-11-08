6ce195cd3f245d7905b35f0051827985fa086e33
nurul-islam-prothomalo <nurul.islam@prothomalo.com>
2023-11-09 00:06:00 +0600

    remove log

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Routes } from 'react-router-dom';
-import getNestedRoutes from '@utils/getNestedRoutes';
 import Page from '@components/common/helmet/helmet';
 import RouteList from '@client/routes';
+import getNestedRoutes from '@utils/getNestedRoutes';
 
 function App() {
   return (
--- src/client/components/pages/home/index.js
+++ src/client/components/pages/home/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 
 function HomePage() {
-  console.log('Hm');
-
   return <h1>Hey this is home epage</h1>;
 }
 
